5eb3f69dd376ba09256f55cbff0c98424b7258a9
Vinicius Farias <fsvinicius9@gmail.com>
2021-05-27 13:22:35 -0300

    React-dom

---
+++ app/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/, exclude: /node_modules/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+      
+     }
+    ]
+  }
+}
