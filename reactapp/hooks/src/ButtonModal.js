a0101d35e57b0141bedb61b132ac150a5154541f
Vinicius Farias <fsvinicius9@gmail.com>
2021-06-08 18:55:41 -0300

    useFetch()

---
+++ reactapp/hooks/src/ButtonModal.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// const ButtonModal = ({setModal}) => {
+
+//   function handleClick () {
+//     setModal((ativo)=> !ativo)
+//   }
+//    return (
+//     <button onClick={() => setModal(true)}>
+//       Abrir
+//     </button>
+//   )
+// }
+
+// export default ButtonModal
